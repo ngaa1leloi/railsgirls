8e5e6fd62db921eeef36faf62defdcef29fd19ec
ngaa1leloi <ngaa1leloi@gmail.com>
2018-04-22 05:00:52 +0000

    10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/info'
-root 'ideas#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  resources :comments
+  root 'pages#info'
+#  root 'home#index'
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
